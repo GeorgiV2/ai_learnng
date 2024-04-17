a0a769e082f674f08f7fcabe861fbdc3185ea1d4
Georgi Vasilev <48720399+GeorgiV2@users.noreply.github.com>
2024-04-18 00:10:31 +0300

    Delete AI/zadachi directory

--- AI/zadachi/OOP/Cars/main.py
+++
@@ -1,8 +0,0 @@
-
-from vehicle import Car
-
-rs7 =  Car("rs7","Audi",240,"black")
-name = rs7.name
-print (rs7.start(), name,rs7.model)
-rs7.drive()
-
--- AI/zadachi/OOP/Task_managing_system/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self,id:int, name:str,role:str,email:str,assigned_tasks=None):
-        self.id = id
-        self.name = name
-        self.role = role
-        self.email = email
-        self.assigned_tasks = assigned_tasks
